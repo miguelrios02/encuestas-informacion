60ff6314c4eb13c2c8fcf4037ba4c95d45e246e3
Leonardo Melchiori <91997494+leomel266@users.noreply.github.com>
2023-03-19 17:22:33 -0300

    feat: footer

--- components/navigation/footer/Footer.tsx
+++ components/navigation/footer/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
-    <div className='min-h-[250px] w-700 flex justify-center items-center flex-col bg-[url("/footer-banner.png")] bg-cover bg-no-repeat bg-center -mt-4'>
-      <div className="flex flex-col">
+    <div className='min-h-[488px] flex justify-center items-center flex-col bg-[url("/footer-banner.png")] bg-cover bg-no-repeat bg-center -mt-4'>
+      <div className="flex flex-col mt-36">
         <input
           className="px-6 py-4 rounded-3xl w-full sm:w-[465px]"
           type="text"
