56037b2317443221675b92bb31bf654a54e332f4
Leonardo Melchiori <91997494+leomel266@users.noreply.github.com>
2023-03-23 22:25:54 -0300

    feat: loginpage

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,7 +1,7 @@
-export default function Login() {
-  return (
-    <div>
-      <h1>login</h1>
-    </div>
-  );
-}
+import { NextPageWithLayout } from '../page';
+
+const LoginPage: NextPageWithLayout = () => {
+  return <div>Login</div>;
+};
+
+export default LoginPage;
