589d8bf6affacfcc746a813c930374bb50d24450
Leonardo Melchiori <91997494+leomel266@users.noreply.github.com>
2023-04-04 17:51:36 -0300

    feat: view

--- pages/publications/index.tsx
+++ pages/publications/index.tsx
@@ -1,23 +1,38 @@
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import Logo from '../../components/assets/logo/Logo';
-import { IPublications } from '../../lib/interfaces/publications.interface';
+import { categories } from '../../lib/data/categories';
+import { categoriesPublications } from '../../lib/data/categoriesPublications.mock';
+
+type FormData = {
+  title: string;
+  type: string;
+  category: string;
+  whyRecommend: string;
+  referenceLink: string;
+  images: FileList | null;
+};
 
 export default function CreatePublications() {
-  const { register, handleSubmit } = useForm({
-    defaultValues: {
-      title: '',
-      type: '',
-      category: '',
-      review: '',
-      url: '',
-      photo: '',
-    },
-  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
-  const [publicData, setPublicData] = useState<IPublications>();
+  const [agregarImagenes, setAgregarImagenes] = useState(false);
 
-  const onSubmit = async (data: IPublications) => {
+  const handleSiguiente = () => {
+    setAgregarImagenes(true);
+  };
+
+  const handleBack = () => {
+    setAgregarImagenes(false);
+  };
+
+  const [publicData, setPublicData] = useState<FormData>();
+
+  const onSubmit = async (data: FormData) => {
     console.log(data);
     setPublicData(data);
     console.log(publicData);
@@ -37,39 +52,83 @@ export default function CreatePublications() {
       <section>
         <button>Back</button>
         <div>barra de carga</div>
-        <section>
-          <h1>publicacion</h1>
-          <p>informacion basica</p>
+        {!agregarImagenes ? (
+          <section>
+            <h1>publicacion</h1>
+            <p>informacion basica</p>
 
-          <form className="flex" onSubmit={handleSubmit(onSubmit)}>
-            <label>
-              <span className="font-semibold">Title</span>
-              <input
-                className="px-3 rounded-3xl bg-white text-[#A7A6A7] p-1"
-                type="text"
-                {...register('title')}
-              />
-            </label>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <label>
+                Título:
+                <input type="text" {...register('title', { required: true })} />
+                {errors.title && <span>Este campo es requerido</span>}
+              </label>
+              <br />
 
-            <label>
-              <span>Type</span>
-              <input type="text" {...register('type')} />
-            </label>
-            <label>
-              <span className="font-semibold">category</span>
-              <input type="text" {...register('category')} />
-            </label>
-            <label>
-              <span className="font-semibold">review</span>
-              <input type="text" {...register('review')} />
-            </label>
-            <label>
-              <span className="font-semibold">Link</span>
-              <input type="text" {...register('url')} />
-            </label>
-            <button>enviar</button>
-          </form>
-        </section>
+              <label>
+                Tipo:
+                <select {...register('type', { required: true })}>
+                  {categories.map((item) => {
+                    return (
+                      <option key={item.id} value={item.name}>
+                        {item.name}
+                      </option>
+                    );
+                  })}
+                </select>
+                {errors.type && <span>Este campo es requerido</span>}
+              </label>
+              <br />
+
+              <label>
+                Categoría:
+                <select {...register('category', { required: true })}>
+                  {categoriesPublications.map((item) => {
+                    return (
+                      <option key={item.id} value={item.name}>
+                        {item.name}
+                      </option>
+                    );
+                  })}
+                </select>
+                {errors.category && <span>Este campo es requerido</span>}
+              </label>
+              <br />
+
+              <label>
+                ¿Por qué lo recomiendas?
+                <textarea {...register('whyRecommend', { required: true })} />
+                {errors.whyRecommend && <span>Este campo es requerido</span>}
+              </label>
+              <br />
+
+              <label>
+                Link de referencia:
+                <input type="text" {...register('referenceLink')} />
+              </label>
+              <br />
+
+              <label>
+                Imágenes:
+                <input
+                  type="file"
+                  {...register('images')}
+                  multiple
+                  accept="image/*"
+                />
+              </label>
+              <br />
+
+              <button onClick={handleSiguiente}>siguiente</button>
+            </form>
+          </section>
+        ) : (
+          <>
+            <p>Agregar imágenes aquí</p>
+            <button type="submit">Enviar</button>
+            <button onClick={handleBack}>Back</button>
+          </>
+        )}
       </section>
     </div>
   );
