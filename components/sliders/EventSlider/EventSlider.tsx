c219a896924325e9cd4dea7f6955dc1ea28f2684
mianriga12 <mianriga@gmail.com>
2023-04-12 10:24:54 -0500

    feat: details of card

--- components/sliders/EventSlider/EventSlider.tsx
+++ components/sliders/EventSlider/EventSlider.tsx
@@ -61,7 +61,6 @@ export const EventSlider: React.FC<IEventSlider> = ({
                 url={event.reference_link}
                 photo={'/Vector.png'}
                 id={event.id}
-                publication_type_id={event.publication_type_id}
               />
             </SwiperSlide>
           ))}
